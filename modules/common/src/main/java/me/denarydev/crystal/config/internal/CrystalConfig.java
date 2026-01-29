cd3e5235c28b5f6f43cf1ae0ec4cdb932f6b342c
DenaryDev <denaryplanet@gmail.com>
2026-01-30 00:04:40 +0500

    Don't add messages-path property to config.yml on Core platform

--- modules/common/src/main/java/me/denarydev/crystal/config/internal/CrystalConfig.java
+++ modules/common/src/main/java/me/denarydev/crystal/config/internal/CrystalConfig.java
@@ -10,6 +10,7 @@ package me.denarydev.crystal.config.internal;
 import io.sapphiremc.lib.configurate.objectmapping.ConfigSerializable;
 import io.sapphiremc.lib.configurate.objectmapping.meta.Comment;
 import me.denarydev.crystal.Crystal;
+import me.denarydev.crystal.Platform;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.nio.file.Path;
@@ -29,7 +30,9 @@ public final class CrystalConfig {
     @Comment("Путь к файлу настроек пулов бд.")
     private Path poolsPath = Crystal.instance().dataFolder().resolve("pools.yml");
     @Comment("Путь к файлу настроек сообщений.")
-    private Path messagesPath = Crystal.instance().dataFolder().resolve("messages.yml");
+    private Path messagesPath = Platform.current() == Platform.CORE ? // На коре сообщения не используются
+        null :
+        Crystal.instance().dataFolder().resolve("messages.yml");
 
     public Path poolsPath() {
         return poolsPath;
